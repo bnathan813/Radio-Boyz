e52a3643c5edaf70dd3656a06cbae5940a442a4a
Nathan Brown <bnathan@vt.edu>
2021-03-08 10:33:59 -0500

    git-commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+  define('DB_HOST', "pamplin-bit2020.mysql.database.azure.com"); 
+  define('DB_USER', "bit4444group41");  
+  define('DB_PASS', "6ydnCWKKzq6hfePh");
+  define('DB_NAME', "bit4444group41");
+  define("DB_PORT", "3306");
+
+?>
