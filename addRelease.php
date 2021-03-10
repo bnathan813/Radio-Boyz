757f552485cc3bcb77a6336efa3cfc7e273b4b22
Nathan Brown <bnathan@vt.edu>
2021-03-10 11:17:21 -0500

    AddRelease PHP Code

--- addRelease.php
+++ addRelease.php
@@ -1,4 +1,46 @@
+<?php
+$artist = "";
+$title ="";
+$label = "";
+$genre = "";
+$author = "";
+$date = date("m/d/y");
+$desc = "";
+$suggested = "";
+$FCC = "";
+$cover = "";
+$error = false;
+if (isset($_POST["submit"])) {
+    if (isset($_POST["artist"])) $artist = $_POST["artist"];
+    if (isset($_POST["title"])) $title = $_POST["title"];
+    if (isset($_POST["label"])) $label = $_POST["label"];
+    if (isset($_POST["genre"])) $genre = $_POST["genre"];
+    if (isset($_POST["addedBy"])) $author = $_POST["addedBy"];
+    if (isset($_POST["addDate"])) $date = $_POST["addDate"];
+    if (isset($_POST["description"])) $desc = $_POST["description"];
+    if (isset($_POST["suggTracks"])) $suggested = $_POST["suggTracks"];
+    if (isset($_POST["FCCTracks"])) $FCC = $_POST["FCCTracks"];
+    if (isset($_POST["cover"])) $cover = $_POST["cover"];
+    if (empty($artist) || empty($title) || empty($label) || empty($genre) || empty($author) || empty($date) || empty($desc) || empty($suggested) || empty($FCC) || empty($cover)) {
+        $error = true;
+    }
+    if (!$error) {
+        require_once("db.php");
+        $sql = "insert into bit4444group41.record(Artist, Title, Label, Genre, Author, DateAdded, Description, Suggested, FCC, AlbumCover)
+        values ('$artist', '$title', '$label', '$genre', '$author', '$date', '$desc', '$suggested', '$FCC', '$cover')";
+        $result = $mydb ->query($sql);
+        if ($result == 1) {
+            echo "<script>alert('Record has been successfully added... Redirecting to home page.')
+            window.location.href = 'Main.php'
+            </script>";
+        }
+    }
+    else {
+        echo "<script>alert('Please enter valid inputs.')</script>";
+    }
+}
 
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -44,7 +86,7 @@
             <label for="cover">Cover:</label>
             <input type="file" id="cover" name="cover" accept=".jpg" accept=".png"><br><br>
 
-            <input type="submit" value="Submit">
+            <input type="submit" name = "submit" value="Add Record">
           </form>
     </body>
 </html>
