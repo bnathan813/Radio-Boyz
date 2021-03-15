5bbc35703df8c5b0f8878620fde5b2136eab4b5e
alecvl08 <alecvl08@gmail.com>
2021-03-15 12:06:52 -0400

    very bad rough draft of getting data from db

--- Main.php
+++ Main.php
@@ -3,9 +3,7 @@
     require_once("db.php");
     $sql = "select * from bit4444group41.record";
     $result = $mydb->query($sql);
-    while ($row = mysqli_fetch_array($result)) {
-        echo $row["Artist"];
-    }
+    
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,74 +18,45 @@
         <button class="add">Add Release</button>
 
         <div class="list">
-            <div class="container">
-                <img src="DaftPunk.png" class="child">
-                <div class="child">
-                    <ul>
-                        <li>
-                            <span class="artist">Daft Punk</span> - <span class="title">Random Access Memories</span>
-                        </li>
-                        <li>
-                            Columbia
-                        </li>
-                        <li>
-                            Electronic
-                        </li>
-                        <li>
-                            Added by: <span class="DJName">Alec</span>
-                        </li>
-                        <li>
-                            3/1/2021
-                        </li>                    
-                    </ul>
-                </div>
-                <div class="child">
-                    <p>
-                        Description: <span class="desc"></span>
-                    </p>
-                </div>
-                <div class="childtracks">
-                    Suggested Tracks: <span class="sugg">1, 2, 3</span> </br>
-                    FCC: <span class="sugg">9</span> </br> </br>
-                    <div class="buttonRemove">
-                        <button class="remove">Remove</button>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <img src="FlyLo.jfif" class="child">
-                <div class="child">
-                    <ul>
-                        <li>
-                            <span class="artist">Flying Lotus</span> - <span class="title">Cosmogramma</span>
-                        </li>
-                        <li>
-                            Warp
-                        </li>
-                        <li>
-                            Electronic
-                        </li>
-                        <li>
-                            Added by: <span class="DJName">Alec</span>
-                        </li>
-                        <li>
-                            3/3/2021
-                        </li>                    
-                    </ul>
-                </div>
-                <div class="child">
-                    <p>
-                        Description: <span class="desc">dank</span>
-                    </p>
-                </div>
-                <div class="childtracks">
-                    Suggested Tracks: <span class="sugg">1, 2, 3</span> </br>
-                    FCC: <span class="sugg">7</span> </br> </br>
-                    <div class="buttonRemove">
-                        <button class="remove">Remove</button>
-                    </div>
-                </div>
-            </div>
+            <?php
+                while ($row = mysqli_fetch_array($result)) {
+                    echo "<div class='container'>";
+                        echo "<img src=".$row['AlbumCover']." class='child'>";
+                        echo "<div class='child'>
+                            <ul>
+                                <li>";
+                                    echo "<span class='artist'>".$row['Artist']."</span> - <span class='title'>".$row['Title']."</span>";
+                                echo "</li>
+                                <li>";
+                                    echo $row['Label'];
+                                echo "</li>
+                                <li>";
+                                    echo $row['Genre'];
+                                echo "</li>
+                                <li>
+                                    Added by: <span class='DJName'>";echo $row['Author']; echo "</span>
+                                </li>
+                                <li>";
+                                    echo $row['DateAdded'];
+                                echo "</li>                    
+                            </ul>
+                        </div>";
+                        /*
+                        <div class="child">
+                            <p>
+                                Description: <span class="desc"></span>
+                            </p>
+                        </div>
+                        <div class="childtracks">
+                            Suggested Tracks: <span class="sugg">1, 2, 3</span> </br>
+                            FCC: <span class="sugg">9</span> </br> </br>
+                            <div class="buttonRemove">
+                                <button class="remove">Remove</button>
+                            </div>
+                        </div>
+                    </div> */
+                }
+            ?>
         </div>
     </body>
 </html>
