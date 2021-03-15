b52818583561562cacd63cfe61e02b82bac94b52
alecvl08 <alecvl08@gmail.com>
2021-03-15 15:56:25 -0400

    Main with better php, no image

--- Main.php
+++ Main.php
@@ -1,4 +1,3 @@
-
 <?php
     require_once("db.php");
     $sql = "select * from bit4444group41.record";
@@ -19,41 +18,44 @@
 
         <div class="list">
             <?php
-                while ($row = mysqli_fetch_array($result)) {
-                    echo "<div class='container'>";
-                        echo "<img src=".$row['AlbumCover']." class='child'>";
-                        echo "<div class='child'>
-                            <ul>
-                                <li>";
-                                    echo "<span class='artist'>".$row['Artist']."</span> - <span class='title'>".$row['Title']."</span>";
-                                echo "</li>
-                                <li>";
-                                    echo $row['Label'];
-                                echo "</li>
-                                <li>";
-                                    echo $row['Genre'];
-                                echo "</li>
-                                <li>
-                                    Added by: <span class='DJName'>";echo $row['Author']; echo "</span>
-                                </li>
-                                <li>";
-                                    echo $row['DateAdded'];
-                                echo "</li>                    
-                            </ul>
-                        </div>
-                        <div class='child'>
-                            <p>";
-                                echo "Description: <span class='desc'>".$row['Description']."</span>";
-                            echo "</p>
-                        </div>
-                        <div class='childtracks'>";
-                            echo "Suggested Tracks: <span class='sugg'>".$row['Suggested']."</span> </br>";
-                            echo "FCC: <span class='sugg'>".$row['FCC']."</span> </br> </br>";
-                            echo "<div class='buttonRemove'>
-                                <button class='remove'>Remove</button>
-                            </div>
-                        </div>
-                    </div>";
+                while ($rows = mysqli_fetch_array($result)) {
+            ?>
+            <div class='container'>
+                <img src=<?php echo $rows['AlbumCover']; ?> class='child'>
+                <div class='child'>
+                    <ul>
+                        <li>
+                            <span class='artist'><?php echo $rows['Artist']; ?></span> - <span class='title'><?php echo $rows['Title']; ?></span>
+                        </li>
+                        <li>
+                            <?php echo $rows['Label']; ?>
+                        </li>
+                        <li>
+                            <?php echo $rows['Genre']; ?>
+                        </li>
+                        <li>
+                            Added by: <span class='DJName'><?php echo $rows['Author']; ?></span>
+                        </li>
+                        <li>
+                            <?php echo $rows['DateAdded']; ?>
+                        </li>                    
+                    </ul>
+                </div>
+                            
+                <div class="child">
+                    <p>
+                        Description: <span class="desc"><?php echo $rows['Description']; ?></span>
+                    </p>
+                </div>
+                <div class="childtracks">
+                    Suggested Tracks: <span class="sugg"><?php echo $rows['Suggested']; ?></span> </br>
+                    FCC: <span class="sugg"><?php echo $rows['FCC']; ?></span> </br> </br>
+                    <div class="buttonRemove">
+                        <button class="remove">Remove</button>
+                    </div>
+                </div>
+            </div>
+            <?php
                 }
             ?>
         </div>
