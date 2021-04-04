90e71e593b47c6d3b751922a425c3fc5bc9936d4
Alec VanLandingham <alecvl08@vt.edu>
2021-04-04 13:54:52 -0400

    much improved styling

--- Main.php
+++ Main.php
@@ -9,14 +9,29 @@
     <head>
         <meta charset="UTF-8"/>
         <title>WUVT New Music Rotation</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/bootstrap.min.css" rel="stylesheet" />
         <link rel="stylesheet" type="text/css" href="MainStyles.css"/>
     </head>
     <body>
-        <img src="WUVT.png" class="logo" alt="WUVT logo">
-        <h1>New Music Rotation</h1>
-        <a class="add" href="addRelease.php">Add Release</a>
+        <header>
+            <div class='container-fluid' id="header">
+                <div class='row align-items-center' id="headerRow">
+                    <div class='col-xs-12 col-sm-4 headerElement'>
+                        <img src="WUVT.png" class="logo" alt="WUVT logo">
+                    </div>
+                    <div class='col-xs-12 col-sm-4 headerElement' id="title">
+                        New Music Rotation
+                    </div>
+                    <div class='col-xs-12 col-sm-4 headerElement'>
+                        <a id="add" href="addRelease.php">Add Release</a>
+                    </div>                    
+                </div>
+            </div>
+        </header>
 
-        <div class="list">
+        <div id="releaseList">
             <?php
                 while ($rows = mysqli_fetch_array($result)) {
                     if($rows['AutoRemoveDate'] <= date("Y-m-d") && $rows['AutoRemove'] == 1) {
@@ -29,45 +44,49 @@
                         echo "<meta http-equiv='refresh' content='0;url=Main.php'>";
                     }
             ?>
-            <div class='container'>
-                <img src="covers/<?php echo $rows['AlbumCover'];?>" class='child'>
-                <div class='child'>
-                    <ul>
-                        <li>
-                            <span class='artist'><?php echo $rows['Artist']; ?></span> - <span class='title'><?php echo $rows['Title']; ?></span>
-                        </li>
-                        <li>
-                            <?php echo $rows['Label']; ?>
-                        </li>
-                        <li>
-                            <?php echo $rows['Genre']; ?>
-                        </li>
-                        <li>
-                            Added by: <span class='DJName'><?php echo $rows['Author']; ?></span>
-                        </li>
-                        <li>
-                            <?php echo $rows['DateAdded']; ?>
-                        </li>
-                        <li>
-                            <?php
-                                if($rows['AutoRemove'] == 1) {
-                                    echo "<strong>Auto-Remove on: ".$rows['AutoRemoveDate']."</strong>";
-                                }
-                            ?>
-                        </li>
-                    </ul>
-                </div>
-                            
-                <div class="child">
-                    <p>
-                        Description: <span class="desc"><?php echo $rows['Description']; ?></span>
-                    </p>
-                </div>
-                <div class="childtracks">
-                    Suggested Tracks: <span class="sugg"><?php echo $rows['Suggested']; ?></span> </br>
-                    FCC: <span class="sugg"><?php echo $rows['FCC']; ?></span> </br> </br>
-                    <div class="buttonRemove">
-                        <a class="remove" href="delete.php?del=<?php echo $rows['idRecord']; ?>&cover=<?php echo $rows['AlbumCover']; ?>">Remove</a>
+            <div class='container' id='release'>
+                <div class='row' id='releaseInfo'>
+                    <div class='col-xs-12 col-sm-8 col-md-3'>
+                        <img src="covers/<?php echo $rows['AlbumCover'];?>" class='cover'>
+                    </div>
+                    <div class='col-xs-12 col-sm-4 col-md-2'>
+                        <ul>
+                            <li>
+                                <span class='artist'><?php echo $rows['Artist']; ?></span> - <span class='title'><?php echo $rows['Title']; ?></span>
+                            </li>
+                            <li>
+                                <?php echo $rows['Label']; ?>
+                            </li>
+                            <li>
+                                <?php echo $rows['Genre']; ?>
+                            </li>
+                            <li>
+                                Added by: <span class='DJName'><?php echo $rows['Author']; ?></span>
+                            </li>
+                            <li>
+                                <?php echo $rows['DateAdded']; ?>
+                            </li>
+                            <li>
+                                <?php
+                                    if($rows['AutoRemove'] == 1) {
+                                        echo "<strong>Auto-Remove on: ".$rows['AutoRemoveDate']."</strong>";
+                                    }
+                                ?>
+                            </li>
+                        </ul>
+                    </div>
+                                
+                    <div class='col-xs-12 col-sm-9 col-md-5'>
+                        <p>
+                            Description: <span class="desc"><?php echo $rows['Description']; ?></span>
+                        </p>
+                    </div>
+                    <div class='col-xs-12 col-sm-3 col-md-2'>
+                        Suggested Tracks: <span class="sugg"><?php echo $rows['Suggested']; ?></span> </br>
+                        FCC: <span class="sugg"><?php echo $rows['FCC']; ?></span> </br> </br>
+                        <div class="buttonRemove">
+                            <a class="remove" href="delete.php?del=<?php echo $rows['idRecord']; ?>&cover=<?php echo $rows['AlbumCover']; ?>">Remove</a>
+                        </div>
                     </div>
                 </div>
             </div>
